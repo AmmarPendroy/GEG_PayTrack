02c4825db3953785cc9289317467b10d7161a535
AmmarPendroy <ammar.muhammed@geg-construction.com>
2025-05-31 17:18:45 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from login import login_page
+from router import route_user
+
+# Streamlit page configuration
+st.set_page_config(
+    page_title="GEG PayTrack",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+# Initialize session state for user if not present
+if "user" not in st.session_state:
+    login_page()
+else:
+    route_user()
