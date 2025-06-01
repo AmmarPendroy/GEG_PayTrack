32347f1d3b06bcbc8204717e0bc2cdc32ca1c2cf
AmmarPendroy <ammar.muhammed@geg-construction.com>
2025-06-01 12:15:17 +0300

    Delete utils/db_connection.py

--- utils/db_connection.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-import streamlit as st
-
-# ---------------------------------------
-# Connect to Neon PostgreSQL
-# ---------------------------------------
-def get_db_connection():
-    return psycopg2.connect(st.secrets["DATABASE_URL"])
